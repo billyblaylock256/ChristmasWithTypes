30876f2d33a5ddd20ec4d64b10de3dbf8b712284
Billy Blaylock <billy.blaylock@yahoo.com>
2021-02-11 18:51:42 -0800

    ChristmasWithTypes

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -3,12 +3,12 @@ namespace ChristmasWithTypes
 {
     public class Christmas
     {
-        public string[] Presents;
+        public string[] Presents; 
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        public enum Day {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday /*Fill out the days of the week*/ };
 
     }
 }
